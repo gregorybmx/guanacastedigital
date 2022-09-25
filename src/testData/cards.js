6756f9b3b181fd968293b55d1ef4ba420bb7408a
Kenzerf587 <kendall5872@gmail.com>
2022-09-24 18:49:07 -0600

    Creacion de proyecto y configuracion de carpetas

---
+++ src/testData/cards.js
@@ -0,0 +1,74 @@
+const cards = [
+    {
+        url: "#",
+        title: "Taladro Marca Drill",
+        thumbnail: "../../images/taladro.jpg",
+        category: "Ferreteria",
+        description: "Este es un taladro marca drill, pocas unidades disponibles",
+        price: "30.000",
+    }, 
+
+    {
+        url: "#",
+        title: "Hamburguesa",
+        thumbnail: "../../images/hamburguesa.jpg",
+        category: "Comida",
+        description: "Todo tipo de comida rapida",
+        price: "1.500",
+    },
+
+    {
+        url: "#",
+        title: "Pulseras",
+        thumbnail: "../../images/bisuteria.jpg",
+        category: "Bisuteria",
+        description: "Pulseras de todos los colores y tipos",
+        price: "1.500",
+    },
+
+    {
+        url: "#",
+        title: "Xbox Series X",
+        thumbnail: "../../images/xbox.png",
+        category: "Videojuegos",
+        description: "Xbox Series X Nuevo",
+        price: "400.000",
+    },
+
+    {
+        url: "#",
+        title: "Comida para Perros",
+        thumbnail: "../../images/comidaPerros.png",
+        category: "Animales",
+        description: "Comida para perros Marca Hills",
+        price: "5.500",
+    },
+
+    {
+        url: "#",
+        title: "Tres Leches",
+        thumbnail: "../../images/tres leches.jpg",
+        category: "Comida",
+        description: "Rico tres leches",
+        price: "3.000",
+    },
+
+    {
+        url: "#",
+        title: "Lizano",
+        thumbnail: "../../images/lizano.jpg",
+        category: "Abarrotes",
+        description: "Lizano salsa rosada",
+        price: "1.300",
+    },
+
+    {
+        url: "#",
+        title: "Licores",
+        thumbnail: "../../images/jagermeister.jpg",
+        category: "Licores",
+        description: "Jagermeister con caja",
+        price: "10.000",
+    },
+];
+export default cards;
