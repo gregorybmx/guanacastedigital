cab34b8fc215c1236b1063896e6e18bfece03988
GreivinME <greivin.montoya.estrada@est.una.ac.cr>
2022-10-01 21:06:24 -0600

    Cambios de color, creacion de complementos para simplificar y reutilizar codigo

--- src/App.js
+++ src/App.js
@@ -6,20 +6,21 @@ import UserConfiguration from './pages/UserConfiguration'
 import NavbarComponent from "./components/NavbarComponent";
 import FooterComponent from "./components/FooterComponent";
 import Home from "./testData/cards"
-//import UserConfiguration from './pages/userConfiguration'
 
 function App() {
   return (
     <Router>
       <div className="App">
         <NavbarComponent />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/registro" element={<UserRegister />} />
-          <Route path="/home" element={<Home/>} />
-          <Route path="/configuracionUsuario" element={<UserConfiguration/>}/>
-        </Routes>
+        <div className="container">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/registro" element={<UserRegister />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/userconfiguration" element={<UserConfiguration />} />
+          </Routes>
+        </div>
         <FooterComponent />
       </div>
     </Router>
--- src/testData/cards.js
+++ src/testData/cards.js
@@ -4,47 +4,46 @@ import taladro from '../img/taladro.jpg'
 import hamburguesa from '../img/hamburguesa.jpg'
 import xbox from '../img/xbox.png'
 
-const Cards= [
+const Cards = [
     {
-        id:1,
+        id: 1,
         title: "Taladro",
         category: "Ferreteria",
         image: taladro,
         text: "Taladro Marca Drill",
         textM: "Super Taladro Especial",
         textM2: "A la venta a precio reducido. Local guapo de bajos precios"
-        
+
     },
     {
         id: 2,
         title: "Hamburguesa",
         category: "Comida",
         image: hamburguesa,
-        
+
     },
     {
-        id:3,
+        id: 3,
         title: "Xbox Series X",
         category: "Videojuegos",
         image: xbox,
-        
     },
 ]
 
 function cards() {
-  return (
-    <div className="container d-flex justify-content-center align-items-center h-100">
-        <div className="row">
-            {
-                Cards.map(card => (
-                    <div className="col-md-4" key ={card.id}>
-                        <CardComponent title={card.title} category={card.category} imageSource={card.image} text = {card.text} textN ={card.textM} textN2 ={card.textM2}/>
-                    </div>
-                ))
-            }
+    return (
+        <div className={"container d-flex justify-content-center align-items-center h-100"}>
+            <div className="row">
+                {
+                    Cards.map(card => (
+                        <div className="col-md-4" key={card.id}>
+                            <CardComponent title={card.title} category={card.category} imageSource={card.image} text={card.text} textM={card.textM} textM2={card.textM2} />
+                        </div>
+                    ))
+                }
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default cards
